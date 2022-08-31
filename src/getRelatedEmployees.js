ece552e81ccd3e2d487bc19cd742856764db0370
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-08-31 11:00:10 -0300

    Requisito 4 e não 5 - Corrigindo lint

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -8,11 +8,10 @@ function isManager(id) {
 
 function getRelatedEmployees(managerId) {
   if (isManager(managerId) === false) {
-    throw new Error ('O id inserido não é de uma pessoa colaboradora gerente!');
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
   const colaboradores = employees.filter((employee) => employee.managers.includes(managerId));
   return colaboradores.map((colab) => `${colab.firstName} ${colab.lastName}`);
-  
 }
 console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
