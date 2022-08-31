064b35453aa16c05f493239c1457d66c6cb1215a
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-08-31 16:55:52 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  let obj = {};
+  if (!animal) {
+    const speciesName = data.species.map((element) => element.name);
+    const residentsQuant = data.species.map((element) => element.residents.length);
+    for (let index = 0; index < speciesName.length; index += 1) {
+      obj[speciesName[index]] = residentsQuant[index];
+    }
+  } else if (!animal.sex) {
+    const speciesSex = data.species.filter((element) => element.name === animal.specie);
+    const number = speciesSex[0].residents.length;
+    obj = number;
+  } else {
+    const species2 = data.species.filter((element) => element.name === animal.specie);
+    const number = species2[0].residents.filter((el) => el.sex === animal.sex).length; obj = number;
+  }
+  return obj;
 }
-
+countAnimals();
 module.exports = countAnimals;
