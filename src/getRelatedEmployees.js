6bbd6abcc34bc083fc24f93bb6a24ecbe63171f4
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-08-31 10:52:41 -0300

    Requisito 5

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error ('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const colaboradores = employees.filter((employee) => employee.managers.includes(managerId));
+  return colaboradores.map((colab) => `${colab.firstName} ${colab.lastName}`);
+  
 }
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
