60e6053226cb0d51f4b8bf224b6fc88cf1257b87
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-08-31 20:58:11 -0300

    Requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,38 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('É uma função?', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+  it('Count undefined', () => {
+    expect(handlerElephants(undefined)).toBe(undefined);
+  });
+
+  it('Count not string', () => {
+    expect(handlerElephants(123)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+
+  it('Count elephants number', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+
+  it('Get elephants names', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+
+  it('Get elephants average age', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+
+  it('Get elephants location', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+
+  it('Get elephants popularity', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+
+  it('Test null', () => {
+    expect(handlerElephants('other')).toBe(null);
+  });
+});
