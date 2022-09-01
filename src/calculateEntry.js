95eee09aeee3fb1e272d17dbfe94d0753ae3aac8
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-09-01 17:20:35 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,36 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // Iniciando o Pojeto
-}
+const { prices } = data;
 
-function calculateEntry(entrants) {
-  // seu código aqui
+// const visitors = [
+// {name: 'Lara Carvalho', age: 5},
+// {name: 'Frederico Moreira', age: 5},
+// {name: 'Pedro Henrique Carvalho', age: 5},
+// {name: 'Maria Costa', age: 18},
+// {name: 'Núbia Souza', age: 18},
+// {name: 'Carlos Nogueira', age: 50},
+// ];
+
+function countEntrants(entrants) {
+  const child = entrants.filter((entrant) => entrant.age < 18);
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50);
+  const senior = entrants.filter((entrant) => entrant.age >= 50);
+  const result = {
+    child: child.length,
+    adult: adult.length,
+    senior: senior.length,
+  };
+  return result;
 }
+function calculateEntry(entrants) {
+  if (!entrants || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  const valor = countEntrants(entrants);
+  const total = valor.child * prices.child + valor.adult * prices.adult
+  + valor.senior * prices.senior;
+  return total;
+}
+calculateEntry();
 
 module.exports = { calculateEntry, countEntrants };
