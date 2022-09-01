0f96ee6d7fbb8bca1d6b7291fd5838adaebe9f33
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-09-01 20:13:46 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { employees, species } = data;
 
+function getOldestFromFirstSpecies(id) {
+  const getFirstSpecie = employees.filter((employee) => employee.id === id)[0].responsibleFor[0];
+  const getAnimal = species.filter((specie) => specie.id === getFirstSpecie)[0].residents;
+  const getOlderAnimal = getAnimal.sort((a, b) => b.age - a.age);
+  return [getOlderAnimal[0].name, getOlderAnimal[0].sex, getOlderAnimal[0].age];
+}
 module.exports = getOldestFromFirstSpecies;
